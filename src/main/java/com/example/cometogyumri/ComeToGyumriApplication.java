c7c053e76826aff5d23b6db2c92a067a235949e6
Samvel <samvelkhorotyan@gmail.com>
2022-03-17 17:55:44 +0400

    Initial commit

---
+++ src/main/java/com/example/cometogyumri/ComeToGyumriApplication.java
@@ -0,0 +1,13 @@
+package com.example.cometogyumri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeToGyumriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComeToGyumriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cometogyumri/ComeToGyumriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cometogyumri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeToGyumriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
